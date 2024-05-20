fe981c240824362078ce5d8934896d3d0726d502
yCAp1e <2577022217@qq.com>
2024-05-20 14:23:20 +0800

    bug修复

--- Mail_main.py
+++ Mail_main.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+#
 import requests
 from hashlib import md5
 
